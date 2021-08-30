d593cbc1cd3a3920139b77cd394d2f38e620ed75
micky rendon <elderendon@hotmail.es>
2021-08-30 17:47:01 -0300

    features

--- js/app.js
+++ js/app.js
@@ -4,7 +4,9 @@ const time = ()=>{
     let minutes = date.getMinutes();
     let seconds = date.getSeconds();
 
-    return document.getElementById('hour').innerHTML = `${hour}:${minutes}:${seconds}`;
+    return  document.getElementById('hour').innerHTML = `${hour}`,
+            document.getElementById('minutes').innerHTML = `:${minutes}`,
+            document.getElementById('seconds').innerHTML = `:${seconds}`;
 }
 
 // no necesitamos del onload para que se actualize cada segundo
