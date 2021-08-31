ce1c18166c5792875560397448456979d06fadfb
micky rendon <elderendon@hotmail.es>
2021-08-31 10:19:56 -0300

    fullDate y doble digito en js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,38 @@
+// en esta ocasion estoy usando arrow function
 const time = ()=>{
     let date    = new Date();
-    let hour    = date.getHours();
-    let minutes = date.getMinutes();
-    let seconds = date.getSeconds();
+    let hour    = dobleDigito(date.getHours());
+    let minutes = dobleDigito(date.getMinutes());
+    let seconds = dobleDigito(date.getSeconds());
+
+    fullDate(date);
 
     return  document.getElementById('hour').innerHTML = `${hour}`,
             document.getElementById('minutes').innerHTML = `:${minutes}`,
             document.getElementById('seconds').innerHTML = `:${seconds}`;
 }
+
+const dobleDigito =(hora) =>{
+    if(hora < 10){
+        hora = `0${hora}`;
+    }
+    return hora;
+}
+
+const fullDate = (date) =>{
+    const days = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+    const months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre','Octubre', 'Noviembre', 'Diciembre'];
+    
+    let weekDays = days[date.getDay()];
+    let fecha = date.getDate();
+    let mes   = months[date.getMonth()];
+    let year  = date.getFullYear()
+
+    let dayMonthYear = `${weekDays} ${fecha} ${mes} ${year}`;
+    document.getElementById('text').innerHTML = `${dayMonthYear}`;
+}
+
+
 
 // no necesitamos del onload para que se actualize cada segundo
 setInterval(time, 1000);
